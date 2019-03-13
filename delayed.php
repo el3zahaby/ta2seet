5803f4d6ab651d87ce42331fb811fddb68de3129
Bola Naguib <bolanaguib@gmail.com>
2019-03-14 01:58:02 +0200

    :)

---
+++ delayed.php
@@ -0,0 +1,58 @@
+<?php include'header.php'; ?>
+<section>
+  <div class="" uk-grid>
+<div class="uk-width-1-1">
+  <table class="uk-table  uk-table-justify uk-table-divider">
+    <thead>
+      <tr>
+        <th>م</th>
+        <th>الإسم</th>
+        <th>القسم</th>
+        <th>المنطقة</th>
+        <th>المبلغ</th>
+        <th>ملحوظة</th>
+        <th>رقم التليفون</th>
+        <th>فترة التأخير</th>
+        <th>فترة التأخير</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="table__row">
+        <td>1</td>
+        <td>بولا نجيب إبراهيم</td>
+        <td>السويس</td>
+        <td>مديرية الصحة</td>
+        <td>2000</td>
+        <td>دقع 100ج</td>
+        <td>01023989330</td>
+        <td>30 يوم</td>
+        <td><button class="uk-button uk-button-default" type="button">Button</button></td>
+      </tr>
+      <tr class="table__row">
+        <td>2</td>
+        <td>بولا نجيب إبراهيم</td>
+        <td>السويس</td>
+        <td>مديرية الصحة</td>
+        <td>2000</td>
+        <td>دقع 100ج</td>
+        <td>01023989330</td>
+        <td>30 يوم</td>
+        <td><button class="uk-button uk-button-default" type="button">Button</button></td>
+      </tr>
+      <tr class="table__row">
+        <td>3</td>
+        <td>بولا نجيب إبراهيم</td>
+        <td>السويس</td>
+        <td>مديرية الصحة</td>
+        <td>2000</td>
+        <td>دقع 100ج</td>
+        <td>01023989330</td>
+        <td>30 يوم</td>
+        <td><button class="uk-button uk-button-default" type="button">Button</button></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+  </div>
+</section>
+<?php include'footer.php'; ?>
